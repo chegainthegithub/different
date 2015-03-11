50fd7f41935433aef12ef819c2d942215e863cf5
chegainthegithub <chegevara.4151@gmail.com>
2015-03-12 01:47:14 +0300

    NEW

---
+++ some algorythms/conjunction exhaustion.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int arr[5] = { 1 , 2 , 3 , 4 , 5 };
+	int full_arr[8] = { 1 , 2 , 3 , 4 , 5 , 6 , 7 , 8 };
+	int true_arr[8] = { 0, 1 , 2 , 3 , 4 , 5 , 6 , 7 };
+	int num = 1;
+	int n, m;
+	n = 7;
+	int k = 4;
+	int l = 0;
+	for (int s = 0; s < 5; s++)
+		cout << true_arr[s];
+	cout << endl;
+	int i;
+	while (true)
+	{
+		for (i = k; i >= 0; i--)
+		{
+			while (arr[i] < full_arr[n - l])
+			{
+				arr[i]++;
+				for (int f = i + 1; f <= k; f++)
+					arr[f] = arr[i] + f - i;
+				for (int s = 0; s <= k; s++)
+					cout << true_arr[arr[s] - 1];
+				cout << endl;
+				for (int f = k; f >= i + 1; f--)
+				{
+					while (arr[f] < full_arr[f + n - k])
+					{
+						++arr[f];
+						for (int s = 0; s <= k; s++)
+							cout << true_arr[arr[s] - 1];
+						cout << endl;
+					}
+				}
+			}
+			++l;
+		}
+		if (i == -1)
+			break;
+	}
+	return 0;
+}
